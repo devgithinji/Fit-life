adaae3995adde27c4466604fe8b9af5c12adae3c
dennis githinji <wakahiad@gmail.com>
2022-08-25 01:44:13 +0300

    final commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+'use strict'
+//add event on element
+const addEventOnElement = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback)
+        }
+    } else {
+        elem.addEventListener(type, callback)
+    }
+}
+
+//navbar toggle
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]")
+const navLinks = document.querySelectorAll("[data-nav-link]")
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active")
+}
+
+addEventOnElement(navTogglers, "click", toggleNavbar)
+
+//back to top
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active")
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active")
+    }
+})
